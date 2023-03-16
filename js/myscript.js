51d7cb6146ec66f78bd5b7dc0084167e53cbdf3c
G-Lumia <giorgiolumia@gmail.com>
2023-03-16 15:24:03 +0100

    fixed background, added comments

--- js/myscript.js
+++ js/myscript.js
@@ -7,16 +7,23 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+
 const pButton = document.getElementById("sendWord");
 
 pButton.addEventListener("click" , palindromeCheck);
 
+// Tramite l'uso di questa funzione avremo modo di capire se la parola inseirita dall'utente è un palindromo o meno
+
 function palindromeCheck()
 {
+    // variabile che contiente il testo inserito dall'utente
     const word = document.getElementById("palindrome").value.toLowerCase();
+    // array di caratteri che conterrà le lettere di word
     const charArray = word.split("");
+    // inizializzazione di un vettore che conterrà le lettere di word, in ordine inverso
     const reverseArray = [];
     let index = 0;
+    // controllo per verificare la validità dell'input
     if(!isNaN(word))
     {
         document.getElementById("palindrome-answer").classList.remove("d-none");
@@ -24,12 +31,14 @@ function palindromeCheck()
     }
     else
     {
+        // ciclo iterativo che scorrerà al contrario il vettore charArray, usato per creare il vettore reverseArray
         for(let x = charArray.length - 1 ; x >= 0 ; x--)
         {
             reverseArray[index] = charArray [x];
             index++;
         }
     
+        // controllo per verificare se la parola inserita dall'utente è uguale alla parola stessa scritta in ordine invertito
         if(word === reverseArray.join(""))
         {
             document.getElementById("palindrome-answer").classList.remove("d-none");
@@ -52,13 +61,24 @@ const gButton = document.getElementById("startGame");
 
 gButton.addEventListener("click" , oddOrEven);
 
+// Tramite l'uso di questa funzione giocheremo a "pari e dispari" contro il computer
+
 function oddOrEven(){
+
+    // variabile che conterrà il valore scelto dall'utente
     const userNumber = parseInt(document.getElementById("userNum").value);
+    // variabile che conterrà "pari" o "dispari", scelto dall'utente
     const userChoice = document.getElementById("pari-dispari").value;
+    // variabile che conterrà il valore scelto a caso dal computer
     const pcNumber = Math.floor(Math.random() * 5) + 1;
+
+    //i due numeri vengono sommati per dei futuri controlli
     const sum = userNumber + pcNumber;
+
+    //variabile che conterrà l'esito della partita
     let answer;
 
+    // controllo per verificare se la somma dei due numeri è pari o dispari
     if(sum % 2 == 0)
     {
         answer = "pari";
@@ -68,6 +88,7 @@ function oddOrEven(){
         answer = "dispari";
     }
 
+    // controllo per verificare se l'utente ha indovinato l'esito del gioco o meno
     if(userChoice === answer)
     {
         document.getElementById("gameResult").classList.remove("d-none");
